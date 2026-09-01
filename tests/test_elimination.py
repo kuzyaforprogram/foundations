804d97c7f75846fc5eec3067a5dc989f13ecdf1a
kuzyaforprogram <kuzyaforprogram@gmail.com>
2026-09-01 15:13:44 +0300

    Add lu factorization with test suite

--- tests/test_elimination.py
+++ tests/test_elimination.py
@@ -19,3 +19,35 @@ def test_l_is_unit_lower_triangular():
     assert np.allclose(L, np.tril(L))
     assert np.allclose(1, np.diag(L))
 
+def test_non_square_raises():
+    A = np.array([[1., 3., 0.], [2., 4., 0.]])
+    with pytest.raises(ValueError):
+        lu(A)
+
+def test_zero_pivots():
+    A = np.array([[0., 3., 1.], [2., 4., 0.], [2., 0., 1.]])
+    with pytest.raises(ValueError):
+        lu(A)
+
+def test_different_A_sizes():
+    rng = np.random.default_rng(0)
+
+    for n in range(3, 9):
+        A = rng.normal(size=(n, n))
+        L, U = lu(A)
+        assert np.allclose(L @ U, A)
+
+def test_A_unchanged():
+    A = np.array([[1., 3., 0.], [2., 4., 0.], [2., 0., 1.]])
+    A_before = A.copy()
+    lu(A)
+    assert np.array_equal(A, A_before)
+
+def test_correct_type():
+    A = np.array([[2, 1], [6, 4]])
+    L, U = lu(A)
+    assert U.dtype == float
+    assert np.allclose(L @ U, A)
+
+
+   
