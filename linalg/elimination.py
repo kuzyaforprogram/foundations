dd39cf888bcdace150d2c7dc4c061fe83d60a282
kuzyaforprogram <kuzyaforprogram@gmail.com>
2026-09-01 16:10:56 +0300

    Add plu with partial pivoting and tests

--- linalg/elimination.py
+++ linalg/elimination.py
@@ -26,3 +26,39 @@ def lu(A):
             U[i, :] = U[i, :] - m * U[k, :]
             L[i, k] = m
     return L, U
+
+
+def plu(A):
+
+    A = np.asarray(A)
+
+    if A.shape[0] != A.shape[1]:
+
+        raise ValueError("WRONG SHAPE")
+
+    n = A.shape[0]
+
+    U = np.array(A, dtype=float)
+
+    L = np.eye(n)
+
+    P = np.eye(n)
+
+    for k in range(n):
+        bp = np.argmax(np.abs(U[k:, k])) + k
+        if bp != k:
+            P[[k, bp]] = P[[bp, k]]
+            U[[k, bp]] = U[[bp, k]]
+            L[[k, bp], :k] = L[[bp, k], :k]
+
+
+        if abs(U[k, k]) < 1e-12:
+                    raise ValueError(f"The pivot at {k} is zero, cannot perform LU decomposition.")
+
+        for i in range(k+1, n):
+            m = U[i, k] / U[k, k]
+            U[i, :] = U[i, :] - m * U[k, :]
+            L[i, k] = m
+
+    
+    return L, U, P
